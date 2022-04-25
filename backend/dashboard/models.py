0cc0e48bcc312de7e1ccc879a617e4b690f18e16
claudia <xclaudiaarias@gmail.com>
2022-04-24 21:30:45 -0400

    some ficing to the database models.

--- backend/dashboard/models.py
+++ backend/dashboard/models.py
@@ -1,4 +1,5 @@
 from datetime import datetime
+import datetime
 from pyexpat import model
 from tkinter import CASCADE
 from unicodedata import category
@@ -14,26 +15,29 @@ class User(models.Model):
     email = models.EmailField(max_length=50)
     password = models.CharField(max_length=15)
     created_at = timezone.now()
-
-class Transaction(models.Model):
-    user = models.ForeignKey(User, default=1, on_delete=models.CASCADE)
-    category = models.CharField(max_length=30)
-    transc_name = models.CharField(max_length=30)
-    transaction_date = timezone.now()
-    amount_paid = models.DecimalField(default=0, max_digits=10, decimal_places=2)
     
 class Bills(models.Model):
     user = models.ForeignKey(User, default=1, on_delete=models.CASCADE)
-    transaction = models.ForeignKey(Transaction, default=1, on_delete=models.CASCADE)
+    category = models.CharField(max_length=30, default="category")
+    bill_name = models.CharField(max_length=30, default="bill name")
     amount_due = models.DecimalField(default=0, max_digits=10, decimal_places=2)
     due_date = forms.DateField()
-    isPaid = models.BooleanField(default=True)
+    isPaid = models.BooleanField(default=False)
     pay_date = forms.DateField()
     
+    
+# probably will have to delete bills from transaction 
+# include category and all that locally 
+class Transaction(models.Model):
+    user = models.ForeignKey(User, default=1, on_delete=models.CASCADE)
+    category = models.CharField(max_length=30, default="category")
+    bill_name = models.CharField(max_length=30, default="bill name")
+    transaction_date = timezone.now()
+    amount_paid = models.DecimalField(default=0, max_digits=10, decimal_places=2)
 class Budget(models.Model):
     user = models.ForeignKey(User, default=1, on_delete=models.CASCADE)
-    transaction = models.ForeignKey(Transaction, default=1, on_delete=models.CASCADE)
-    category = models.CharField(max_length=30, default="Caterogy")
+    category = models.CharField(max_length=30, default="Category")
+    amount_saved = models.DecimalField(default=0, max_digits=10, decimal_places=2)
     budget_goal = models.DecimalField(default=0, max_digits=10, decimal_places=2)
     created_at = timezone.now()
 
@@ -42,29 +46,29 @@ class Income(models.Model):
     income_name = models.CharField(max_length=20, default="income_name")
     created_at = timezone.now()
     deposit = models.DecimalField(default=0, max_digits=10, decimal_places=2)
-    total_amount = models.DecimalField(default=0, max_digits=10, decimal_places=2)
     updated_at = models.DateTimeField('updated_at', auto_now=True)
 
 class Balance(models.Model):
     user = models.ForeignKey(User, default=1, on_delete=models.CASCADE)
     income = models.ForeignKey(Income, on_delete=models.CASCADE)
     transaction = models.ForeignKey(Transaction, default=0, on_delete=models.CASCADE)
-    date = models.DateField()
-    updated_at = timezone.now()
     difference = models.DecimalField(default=0, max_digits=10, decimal_places=2)
+    date_bal = models.DateTimeField('balance_date', auto_now=True)
+    updated_at = timezone.now()
     
 class Savings(models.Model):
     user = models.ForeignKey(User, default=1, on_delete=models.CASCADE)
     transaction = models.ForeignKey(Transaction, default=1, on_delete=models.CASCADE)
-    income = models.ForeignKey(Income, default=0, on_delete=models.CASCADE)
     deposit_amount = models.DecimalField(default=0, max_digits=10, decimal_places=2)
     total_amount = models.DecimalField(default=0, max_digits=10, decimal_places=2)
     updated_at = timezone.now()
     
+    
+    
+# maybe goals should reference transcations 
 class Goals(models.Model):
     user = models.ForeignKey(User, default=1, on_delete=models.CASCADE)
-    category = models.CharField(max_length=30, default="category")
-    updated_at = timezone.now()
     goal_name = models.CharField(max_length=30, default="goal_name")
     amount_to_save = models.DecimalField(default=0, max_digits=10, decimal_places=2)
     amount_saved = models.DecimalField(default=0, max_digits=10, decimal_places=2)
+    updated_at = timezone.now()
